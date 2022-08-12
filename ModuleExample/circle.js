3f804c1a5c69b5f70cde32e3611e40c4a6f666db
Kerim Ozturk <49280604+Kerimozturkk@users.noreply.github.com>
2022-08-12 09:16:19 +0300

    Add files via upload

---
+++ ModuleExample/circle.js
@@ -0,0 +1,12 @@
+const circleArea = (r) => {
+    return Math.PI * r * r;
+}
+
+const circleCircumference  = (r) => {
+    return Math.PI * 2 * r;
+}
+
+export {
+    circleArea,
+    circleCircumference
+}
---
+++ ModuleExample/index.js
@@ -0,0 +1,4 @@
+import {circleArea,circleCircumference} from './circle.js'
+
+console.log(circleArea(5));
+console.log(circleCircumference(5));
